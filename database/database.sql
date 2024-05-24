e0e061c16b40ae625053881318e1f7ef837674bb
Bryan Machado <bryan.machado.soares@gmail.com>
2024-05-24 01:10:43 -0300

    changes to database

--- database/database.sql
+++ database/database.sql
@@ -5,76 +5,78 @@ SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
 
 -- -----------------------------------------------------
--- Schema appMeioAmbiente
+-- Schema appmeioambiente
 -- -----------------------------------------------------
 
 -- -----------------------------------------------------
--- Schema appMeioAmbiente
+-- Schema appmeioambiente
 -- -----------------------------------------------------
-CREATE SCHEMA IF NOT EXISTS `appMeioAmbiente` DEFAULT CHARACTER SET utf8 ;
-USE `appMeioAmbiente` ;
+CREATE SCHEMA IF NOT EXISTS `appmeioambiente` DEFAULT CHARACTER SET utf8 ;
+USE `appmeioambiente` ;
 
 -- -----------------------------------------------------
--- Table `appMeioAmbiente`.`ecoponto`
+-- Table `appmeioambiente`.`ecoponto`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `appMeioAmbiente`.`ecoponto` (
+CREATE TABLE IF NOT EXISTS `appmeioambiente`.`ecoponto` (
   `id` INT NOT NULL AUTO_INCREMENT,
   `nome` VARCHAR(45) NOT NULL,
   `cnpj` VARCHAR(20) NOT NULL,
   `email` VARCHAR(45) NULL,
   `telefone` VARCHAR(45) NULL,
   `descricao` VARCHAR(400) NOT NULL,
+  `imagemurl` VARCHAR(400) NULL DEFAULT 'https://img.freepik.com/premium-vector/modern-minimal-found-error-icon-oops-page-found-404-error-page-found-with-concept_599740-716.jpg',
   PRIMARY KEY (`id`))
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `appMeioAmbiente`.`marker`
+-- Table `appmeioambiente`.`marker`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `appMeioAmbiente`.`marker` (
+CREATE TABLE IF NOT EXISTS `appmeioambiente`.`marker` (
   `id` INT NOT NULL AUTO_INCREMENT,
-  `ecoponto_id` INT NOT NULL,
   `longitude` FLOAT NOT NULL,
   `latitude` FLOAT NOT NULL,
+  `ecoponto_id` INT NOT NULL,
   PRIMARY KEY (`id`),
   INDEX `fk_marker_ecoponto_idx` (`ecoponto_id` ASC),
   CONSTRAINT `fk_marker_ecoponto`
     FOREIGN KEY (`ecoponto_id`)
-    REFERENCES `appMeioAmbiente`.`ecoponto` (`id`)
+    REFERENCES `appmeioambiente`.`ecoponto` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `appMeioAmbiente`.`categoria`
+-- Table `appmeioambiente`.`categoria`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `appMeioAmbiente`.`categoria` (
+CREATE TABLE IF NOT EXISTS `appmeioambiente`.`categoria` (
   `id` INT NOT NULL AUTO_INCREMENT,
   `nome` VARCHAR(45) NOT NULL,
   `descricao` VARCHAR(400) NOT NULL,
+  `imagemurl` VARCHAR(400) NULL DEFAULT 'https://img.freepik.com/premium-vector/modern-minimal-found-error-icon-oops-page-found-404-error-page-found-with-concept_599740-716.jpg',
   PRIMARY KEY (`id`))
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `appMeioAmbiente`.`marker_has_categoria`
+-- Table `appmeioambiente`.`marker_has_categoria`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `appMeioAmbiente`.`marker_has_categoria` (
+CREATE TABLE IF NOT EXISTS `appmeioambiente`.`marker_has_categoria` (
   `id` INT NOT NULL AUTO_INCREMENT,
   `marker_id` INT NOT NULL,
   `categoria_id` INT NOT NULL,
+  PRIMARY KEY (`id`),
   INDEX `fk_marker_has_categoria_categoria1_idx` (`categoria_id` ASC),
   INDEX `fk_marker_has_categoria_marker1_idx` (`marker_id` ASC),
-  PRIMARY KEY (`id`),
   CONSTRAINT `fk_marker_has_categoria_marker1`
     FOREIGN KEY (`marker_id`)
-    REFERENCES `appMeioAmbiente`.`marker` (`id`)
+    REFERENCES `appmeioambiente`.`marker` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `fk_marker_has_categoria_categoria1`
     FOREIGN KEY (`categoria_id`)
-    REFERENCES `appMeioAmbiente`.`categoria` (`id`)
+    REFERENCES `appmeioambiente`.`categoria` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
